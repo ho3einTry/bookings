6e50ce60ee226328a93f48844fd2e031266c0ab0
ho3ein <ho3einTry@gmail.com>
2021-05-04 09:50:35 -0700

    add image

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,13 +13,14 @@ func routes(config *config.AppConfig) http.Handler {
 	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 	//return mux
-	mux:= chi.NewRouter()
+	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
 	//mux.Use(WriteToConsole)
 	mux.Use(NoSruve)
 	mux.Use(SessionLoad)
-	mux.Get("/",handlers.Repo.Home)
-	mux.Get("/about",handlers.Repo.About)
-
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*",http.StripPrefix("/static/",fileServer))
 	return mux
 }
