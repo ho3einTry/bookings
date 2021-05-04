6b72ee6210527dec983421f4b114e3bdc28c295a
ho3ein <ho3einTry@gmail.com>
2021-05-04 12:53:42 -0700

    Add Templates

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,6 +20,12 @@ func routes(config *config.AppConfig) http.Handler {
 	mux.Use(SessionLoad)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/contact", handlers.Repo.Contact)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*",http.StripPrefix("/static/",fileServer))
 	return mux
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -51,3 +51,23 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 		StringMap: stringMap,
 	})
 }
+
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.gohtml", &Models.TemplateDate{})
+}
+
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.gohtml", &Models.TemplateDate{})
+}
+
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.gohtml", &Models.TemplateDate{})
+}
+
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.gohtml", &Models.TemplateDate{})
+}
+
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.gohtml", &Models.TemplateDate{})
+}
